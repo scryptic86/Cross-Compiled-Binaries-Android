894df3f59166b3133a8008b9325dbb50afb80135
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-09-16 14:04:51 -0400

    don't remove libs on bin uninstall

--- aria2/dynamic/install.sh
+++ aria2/dynamic/install.sh
@@ -8,3 +8,5 @@ else
   done
 fi
 echo "etc/security/ca-certificates.crt" >> $MODDIR/.$ibinary
+# Since same libs are shared between curl and aria2, don't remove libs on uninstall of binary
+sed -i "/lib/d" $MODDIR/.$ibinary
